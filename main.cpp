4d6176f48d0e8bbb9ed6a8d751060039d26b5175
orcax <siriusxyk@gmail.com>
2015-12-04 03:37:54 -0500

    complete colors

--- main.cpp
+++ main.cpp
@@ -6,18 +6,23 @@
 using namespace cv;
 using namespace std;
 
-#define THRESHOLD_VALUE 200
-#define THRESHOLD_MAX 255
-#define THRESHOLD_TYPE 0
+#define YELLOW 101
+#define GREEN 102
+#define BLUE 103
+#define RED 104
 
+/** Return binarized image of black and white colors.
+ */
 Mat binarize(Mat image_src)
 {
     Mat image_gray, image_binary;
     cvtColor(image_src, image_gray, CV_BGR2GRAY);
-    threshold(image_gray, image_binary, THRESHOLD_VALUE, THRESHOLD_MAX, THRESHOLD_TYPE);
+    threshold(image_gray, image_binary, 200, 255, 0);
     return image_binary;
 }
 
+/** Return list of objects made of set of points.
+ */
 vector<vector<Point> > segment_objects(Mat image_src)
 {
     Mat image_dst;
@@ -60,10 +65,44 @@ vector<vector<Point> > segment_objects(Mat image_src)
     return shapes;
 }
 
+/** Return color of the object on the image.
+ */
+int get_color(Mat image, vector<Point> obj)
+{
+    Vec3b brg = image.at<Vec3b>(obj[0].y, obj[0].x);
+    for(int i=1;i<obj.size();i++)
+    {
+        Vec3b tmp = image.at<Vec3b>(obj[i].y, obj[i].x);
+        brg[0] = (brg[0] * i + tmp[0]) / (i + 1);
+        brg[1] = (brg[1] * i + tmp[1]) / (i + 1);
+        brg[2] = (brg[2] * i + tmp[2]) / (i + 1);
+    }
+    if(brg[0] > 195) return BLUE;
+    if(brg[1] > 195) return RED;
+    if(brg[2] > 195) return GREEN;
+    return YELLOW;
+}
+
+Vec3b get_brg(int color)
+{
+    Vec3b brg(0, 0, 0);
+    switch(color)
+    {
+        case BLUE: 
+            brg[0] = 255; break;
+        case RED: 
+            brg[1] = 255; break;
+        case GREEN: 
+            brg[2] = 255; break;
+        case YELLOW: 
+            brg[1] = 255; brg[2] = 255; break;
+    }
+    return brg;
+}
+
 int main(int argc, char** argv)
 {
-    Mat image;
-    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     if(!image.data) 
     {
         cout << "Could not open image" << endl;
@@ -71,16 +110,13 @@ int main(int argc, char** argv)
     }
 
     Mat image_binary = binarize(image);
-    vector<vector<Point> > contours = segment_objects(image_binary);
-    vector<Point> shape;
+    vector<vector<Point> > objects = segment_objects(image_binary);
     Mat output = Mat::zeros(image_binary.size(), CV_8UC3);
-    for(int i=0;i<contours.size();i++)
+    for(int i=0;i<objects.size();i++)
     {
-        vector<Point> contour = contours[i];
-        Vec3b brg = image.at<Vec3b>(contour[0].y, contour[1].x);
-        Scalar color = Scalar(brg[0], brg[1], brg[2]);
-        //approxPolyDP(contour, shape, arcLength(Mat(contour), true)*0.04, true);
-
+        vector<Point> obj = objects[i];
+        int color = get_color(image, obj);
+        Vec3b brg = get_brg(color);
         /*
         unsigned char r = 255 * (rand()/(1.0 + RAND_MAX));
         unsigned char g = 255 * (rand()/(1.0 + RAND_MAX));
@@ -95,11 +131,11 @@ int main(int argc, char** argv)
         }
         */
         //drawContours(output, contours, i, color, CV_FILLED);
-        cout << contour.size() << endl;
-        for(int j=0;j<contour.size();j++)
+        cout << obj.size() << endl;
+        for(int j=0;j<obj.size();j++)
         {
-            int x = contour[j].x;
-            int y = contour[j].y;
+            int x = obj[j].x;
+            int y = obj[j].y;
             output.at<Vec3b>(y, x)[0] = brg[0];
             output.at<Vec3b>(y, x)[1] = brg[1];
             output.at<Vec3b>(y, x)[2] = brg[2];
